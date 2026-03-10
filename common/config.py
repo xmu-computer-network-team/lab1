c3258ce1f85090d8db6d3e2ae828d8aeb3c257a9
jadeproheshan <jadeproheshan@gmail.com>
2026-03-10 19:52:46 +0800

    (feat):定义常量，标识图图案，作画函数

---
+++ common/config.py
@@ -0,0 +1,10 @@
+# 帧参数
+#sdsa
+BLOCK_SIZE: int = 8#sd:w
+
+
+
+#定位图案
+FINDER_SIZE = 7
+ALIGN_SIZE = 5
+SEPARATOR_WIDTH = 1
---
+++ encoder/frame_builder.py
@@ -0,0 +1,12 @@
+import common.config
+import common.pattern
+
+class FrameBuilder:
+    def _init_(self):
+    
+
+    def draw_finder(self):
+        #外黑内白中间3x3黑
+        pattern = generate_finder_pattern()
+        for r in range(FINDER_SIZE)
+        
